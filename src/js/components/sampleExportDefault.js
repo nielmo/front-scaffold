3790183db20a9cc078b295011aca4370ffdbec7f
nielmo <roniel.labrador@gmail.com>
2019-09-19 16:40:40 +0800

    Add comments in functions

--- src/js/components/sampleExportDefault.js
+++ src/js/components/sampleExportDefault.js
@@ -1,3 +1,7 @@
+/**
+ * sample comment
+ * we should always describe what a function do
+ */
 export default () => {
-  console.log('this is an example exported function');
-};
+  console.log('this is an example exported function')
+}
